cadd91f0b8c46aebbf3d0d024068f6c09420f36a
Prasanth K M <prasanthkm49@gmail.com>
2024-08-02 16:25:53 +0530

    corrections

--- lib/profile_config/controller/subscription_plan_controller.dart
+++ lib/profile_config/controller/subscription_plan_controller.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:diet_diet_done/auth/sign_up/view/otp_succuss_screen.dart';
 import 'package:diet_diet_done/profile_config/api_service/create_subscription_service.dart';
 import 'package:diet_diet_done/profile_config/api_service/get_subscription_plan_service.dart';
 import 'package:diet_diet_done/profile_config/controller/plan_categories_controller.dart';
@@ -15,6 +16,8 @@ class SubscriptionPlanController extends GetxController {
   RxString transactionUrl = ''.obs;
   RxString redirectUrl = ''.obs;
   RxString paymentUrl = ''.obs;
+  RxString paymentCheckUrl = ''.obs;
+
   RxBool isSelectedSubscriptionCard = false.obs;
   RxInt subscriptionId = 0.obs;
   RxInt subscriptionCardIdx = 0.obs;
@@ -53,12 +56,17 @@ class SubscriptionPlanController extends GetxController {
     }
   }
 
-  Future<void> getSubscriptionDetails() async {
+  Future<void> getSubscriptionDetails(bool isNav) async {
     try {
       isLoading.value = true;
       await CreateSubscriptionAPiService()
           .getSubscriptionDetails()
           .then((value) => subscriptionDetails.value = value);
+
+      if(isNav){
+        Get.off(const OTPSuccessScreen(screenName: true));
+      }
+
     } catch (e) {
       log("Error while fetching subscription details: $e");
     } finally {
